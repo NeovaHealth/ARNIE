98f129f9d81c593ab3adaf6dacc02ada118251ae
gregorlenz <gregorlenz@neovahealth.co.uk>
2015-11-10 11:58:42 +0000

    cleaned up cucumber connector

--- src/test/cucumber/support/CamelConnector.groovy
+++ src/test/cucumber/support/CamelConnector.groovy
@@ -6,12 +6,9 @@ import org.apache.camel.ExchangePattern
 /**
  * Created by gregorlenz on 16/09/15.
  */
-import org.apache.camel.component.mock.MockEndpoint
 import org.apache.camel.test.spring.CamelSpringTestSupport
 import org.springframework.context.support.AbstractXmlApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
-import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.Resource
 
 class CamelConnector extends CamelSpringTestSupport{
 
@@ -30,28 +27,10 @@ class CamelConnector extends CamelSpringTestSupport{
         //this is key to test routes of a camelContext properly
         this.setUp()
 
-        MockEndpoint admitEndpoint = getMockEndpoint("mock:direct:admit")
-        admitEndpoint.expectedMessageCount(1)
-        admitEndpoint.setResultWaitTime(3000)
-
         Message answer = template.sendBody("direct:hl7listener", ExchangePattern.InOut, input)
         template.sendBody("direct:msgLogging", input)
 
-        admitEndpoint.assertIsSatisfied()
-        assertMockEndpointsSatisfied()
         return answer
     }
-
-    void testPostgres() {
-        Resource input = new ClassPathResource("/msg-08.hl7")
-
-        MockEndpoint msgHistory = getMockEndpoint("mock:direct:msgLogging")
-        msgHistory.expectedMessageCount(1)
-
-        template.sendBody("direct:msgLogging", input.getInputStream())
-        assertMockEndpointsSatisfied()
-
-    }
-
 
 }

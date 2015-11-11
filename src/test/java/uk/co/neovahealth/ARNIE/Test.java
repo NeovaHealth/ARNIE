63b09faf03511dac5275bb7adae236d222fc9d24
gregorlenz <gregorlenz@neovahealth.co.uk>
2015-11-11 16:37:25 +0000

    added Test class in Java package for maven test goals

---
+++ src/test/java/uk/co/neovahealth/ARNIE/Test.java
@@ -0,0 +1,20 @@
+package uk.co.neovahealth.ARNIE;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created by gregorlenz on 11/11/15.
+ */
+public class Test {
+    @Before
+    public void setUp(){
+        //something happens beforehand
+    }
+
+    @After
+    public void tearDown(){
+        //something happens afterwards
+    }
+
+}
